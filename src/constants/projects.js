9ca4d19af17f093ca342df79d507fb713171aa1c
Leonel Tammaro <tammaroleonel@gmail.com>
2022-08-24 01:17:45 -0300

    change description projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,18 +3,18 @@ import mobileAnimalFinder from "../images/animalFinderMobile.jpeg"
 
 export const projects = [
   {
-    proyectImage: mobiletodolist,
-    repo: "https://github.com/leotammaro/to-do-list-frontend",
-    description:
-      'Uno de mis primeros proyectos que realice. "Un to do list", en el cual tenemos 3 columnas que dividen el proceso de nuestras tareas. Las tareas las podemos editar, borrar o bien arrastrar hacia otra columna. Para realizar este proyecto, utilice para el front end React JS y la libreria Chakra UI.Para el back end,utilice Mongo DB, mongoose, Node JS y express. De la misma forma utilice un sistema de autenticación con Firebase para que el usuario pueda logearse desde cualquier dispositivo.  ',
-    title: "To Do List",
-    urlProyect: "https://to-do-list-frontend-ten.vercel.app/",
-  },
-  {
     proyectImage: mobileAnimalFinder,
     title: "Pet Finder",
     urlProyect: "https://dog-client.vercel.app/app",
     repo: "https://github.com/leotammaro/dog-client",
-    description: "Este es uno de mis proyectos mas interesantes que realice. Se trata de un 'Animal Finder'. Basicamente la idea de este proyecto es reportar en el sitio cuando encontramos un animal perdido en la calle. En el mismo reporte, podemos agregar distintas caracteristicas y detalles del animal, por ejemplo: dirección donde fue encontrado, tamaño del animal, color del animal, numero de contacto, etc..  Asi como tambien, veremos que hay un mapa donde nos mostraran los reportes cercanos a nuestra ubicación. Realice tanto el front end, como el back end."
+    description: "Animal Finder es un sitio web donde los usuarios pueden reportar animales perdidos que encuentran en la calle, incluyendo sus características y dirección donde fue encontrado.Este proyecto utiliza la API de google maps para visualizar los reportes cercanos a nuestra ubicación.Utilicé React con ChakraUI  para la interfaz de usuario y agregué pruebas unitarias y de presentación utilizando Jest y Cypress.io. Para el Back-end utilicé node.js junto con express y MongoDB como base de datos; testeé la aplicación con Supertest y Jest."
+  },
+  {
+    proyectImage: mobiletodolist,
+    repo: "https://github.com/leotammaro/to-do-list-frontend",
+    description:
+      '"Un to do list",tenemos 3 columnas que dividen el proceso de nuestras tareas. Las tareas las podemos editar, borrar o bien arrastrar hacia otra columna. Utilicé React JS y la libreria Chakra UI para el front-end .En el back end,utilice Mongo DB, mongoose, Node JS y express. De la misma forma utilice. Contiene un sistema de autenticación con Firebase para que el usuario pueda logearse desde cualquier dispositivo.  ',
+    title: "To Do List",
+    urlProyect: "https://to-do-list-frontend-ten.vercel.app/",
   }
 ];
